cb66bf6b055a9cbc32918e362b5d1d24915b31a8
king_ing_e <2022011870@cju.ac.kr>
2025-05-11 14:25:48 +0900

    commit

--- BE/src/main/java/org/example/xviewer/controller/CaseController.java
+++ BE/src/main/java/org/example/xviewer/controller/CaseController.java
@@ -6,8 +6,10 @@ import org.example.xviewer.model.EventData;
 import org.example.xviewer.repository.CaseRepository;
 import org.example.xviewer.security.dto.ResMessage;
 import org.example.xviewer.security.dto.logcase.CaseRequest;
+import org.example.xviewer.security.dto.logcase.CaseResultResponse;
 import org.example.xviewer.service.CaseService;
 import org.example.xviewer.service.EventDataService;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,6 +21,7 @@ import java.util.List;
 public class CaseController {
     private final CaseService caseService;
     private final EventDataService eventDataService;
+
     @PostMapping("/create")
     public ResponseEntity<ResMessage> createCase(CaseRequest caseRequest, HttpServletRequest request) {
         ResMessage resMessage=caseService.createCase(caseRequest, request);
@@ -26,12 +29,15 @@ public class CaseController {
         return ResponseEntity.ok(resMessage);
     }
 
-    @GetMapping("/list")
+    @GetMapping("/eventdata")
     public ResponseEntity<List<EventData>> getCase(@RequestParam Long id) {
         return ResponseEntity.ok(eventDataService.findAllEventData(id));
-
     }
 
+    @GetMapping("/list")
+    public ResponseEntity<Page<CaseResultResponse>> getCases(HttpServletRequest request) {
+        return ResponseEntity.ok(caseService.getListCase(request));
+    }
     @PutMapping("/list")
     public ResponseEntity<ResMessage> updateCase(@RequestParam Long id) {
         return null;
