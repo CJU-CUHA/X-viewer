6d1804b13dfd4464c6d6b17f21dc1ab32616937d
king_ing_e <2022011870@cju.ac.kr>
2025-05-11 16:29:50 +0900

    commit

--- BE/src/main/java/org/example/xviewer/model/TimeBaseEntity.java
+++ BE/src/main/java/org/example/xviewer/model/TimeBaseEntity.java
@@ -1,13 +1,19 @@
 package org.example.xviewer.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import lombok.Getter;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 
 @MappedSuperclass
 @Getter
 public abstract class TimeBaseEntity {
-
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime created;
 }
